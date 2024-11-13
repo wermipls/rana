b32b7ac1854fa5486c873920ef435f3f4c00843f
wermi <32251376+wermipls@users.noreply.github.com>
2024-11-13 12:27:46 +0100

    make fallback texture use nearest filtering by default

--- src/gfx_textures.cpp
+++ src/gfx_textures.cpp
@@ -31,7 +31,8 @@ uint32_t create_fallback_texture()
     glGenTextures(1, &tex);
     glBindTexture(GL_TEXTURE_2D, tex);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, size, size, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
     return tex;
 }
