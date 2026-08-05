b4c8a140bfe9fc7488214fee1dd6de672675ceb8
wermi <32251376+wermipls@users.noreply.github.com>
2026-08-05 07:56:45 +0200

    fix some tragic oversights in ring buffer impls.

--- src/containers/bitmask_ringbuf.hpp
+++ src/containers/bitmask_ringbuf.hpp
@@ -9,15 +9,14 @@ template <typename T, size_t sz>
 class BitmaskRingBuf {
     constexpr static size_t next_power_of_2(size_t n) {
         n--;
-        for (size_t i = 1; i < sizeof(size_t); i <<= 1) {
+        for (size_t i = 1; i < sizeof(size_t) * 8; i <<= 1) {
             n |= n >> i;
         }
         n++;
         return n;
     }
 
-    constexpr static size_t mask = next_power_of_2(sz) - 1;
-    constexpr static size_t push_offset = sz & mask;
+    constexpr static size_t mask     = next_power_of_2(sz) - 1;
     constexpr static size_t capacity = next_power_of_2(sz);
 
     size_t offset;
@@ -27,8 +26,8 @@ public:
           T &operator[](size_t i)       { return buf[(i+offset) & mask]; }
     const T &operator[](size_t i) const { return buf[(i+offset) & mask]; }
 
-    void push(const T &v) { buf[(offset + push_offset) & mask] = v; offset++; }
-    void push(T &&v)      { buf[(offset + push_offset) & mask] = v; offset++; }
+    void push(const T &v) { buf[(offset + sz) & mask] = v; offset++; }
+    void push(T &&v)      { buf[(offset + sz) & mask] = v; offset++; }
 };
 
 } // namespace rana
--- src/containers/linear_ringbuf.hpp
+++ src/containers/linear_ringbuf.hpp
@@ -10,15 +10,14 @@ template <typename T, size_t sz>
 class LinearRingBuf {
     constexpr static size_t next_power_of_2(size_t n) {
         n--;
-        for (size_t i = 1; i < sizeof(size_t); i <<= 1) {
+        for (size_t i = 1; i < sizeof(size_t) * 8; i <<= 1) {
             n |= n >> i;
         }
         n++;
         return n;
     }
 
-    constexpr static size_t mask = next_power_of_2(sz) - 1;
-    constexpr static size_t push_offset = sz & mask;
+    constexpr static size_t mask     = next_power_of_2(sz) - 1;
     constexpr static size_t capacity = next_power_of_2(sz) * 2;
 
     size_t offset; // fixme: perhaps replace this with pointer to buf+offset?
@@ -27,8 +26,8 @@ class LinearRingBuf {
 public:
     const T &operator[](size_t i) const { return buf[i+offset]; }
 
-    void push(const T &v) { auto i = (offset + push_offset) & mask; buf[i] = v; buf[i+capacity/2] = v; offset = (offset+1) & mask; }
-    void push(T &&v)      { auto i = (offset + push_offset) & mask; buf[i] = v; buf[i+capacity/2] = v; offset = (offset+1) & mask; }
+    void push(const T &v) { auto i = (offset + sz) & mask; buf[i] = v; buf[i+capacity/2] = v; offset = (offset+1) & mask; }
+    void push(T &&v)      { auto i = (offset + sz) & mask; buf[i] = v; buf[i+capacity/2] = v; offset = (offset+1) & mask; }
 
     const T *data() { return &buf[offset]; }
 };
