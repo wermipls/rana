cb7b8cd30a8fe9aaaf8dd759146edd4e461d733d
wermi <32251376+wermipls@users.noreply.github.com>
2026-09-02 12:23:55 +0200

    distortion: change "shape", add bias and oversampling.
    bias or asymmetry is modelled after misstortion 2 (i don't have logic pro to compare). shape always sounded bad and i never used it so it got replaced with something that seems more useful.

--- src/audio/effects.hpp
+++ src/audio/effects.hpp
@@ -11,6 +11,7 @@
 #include "containers/bitmask_ringbuf.hpp"
 #include "fast_math/pow.hpp"
 #include "fast_math/log.hpp"
+#include "halfband.hpp"
 
 namespace rana {
 namespace audio {
@@ -484,15 +485,21 @@ public:
 };
 
 class Distortion : public Effect {
-    static constexpr auto paramCount = 3;
+    static constexpr auto paramCount = 6;
     const char *paramNames[paramCount] = {
         "amount",
         "mix",
         "mode",
+        "asymmetry",
+        "stereo bias",
+        "oversample",
     };
     float params[paramCount] = {
         0.0,
         1.0,
+        0.0,
+        0.5,
+        0.5,
         0.0,
     };
 
@@ -505,15 +512,27 @@ class Distortion : public Effect {
         Shape,
         Fold,
         BadFold,
-    } mode;
-    SampleStereo gain = 1;
-    SampleStereo dry = 0;
-    SampleStereo wet = 1;
+    } mode = Hardclip;
+    double gain = 1;
+    double asymmetry = 0.5;
+    double stereo_bias = 0;
+
+    Oversampler2x<SampleStereo> oversampler = {};
+
+    static SampleStereo bias(SampleStereo x, SampleStereo offset) {
+        // this is the best way i can think of for now.
+        const auto sign = copysign(1.0, x);
+        const auto pos = max(sign, 0.0);
+        const auto neg = SampleStereo(1.0) - pos;
+        return x * pos * 2.0 * (SampleStereo(1.0) - offset)
+             + x * neg * 2.0 * offset;
+    }
 
 public:
     Distortion()
     {
-        mode = Fold;
+        mode = Hardclip;
+        oversampler.enable(false);
     }
 
     void setAmount(float value)
@@ -523,8 +542,7 @@ public:
 
     void setMix(float value)
     {
-        dry = 1.0f - value;
-        wet = value;
+        oversampler.mix(value);
     }
 
     void setMode(float value)
@@ -542,6 +560,9 @@ public:
             case 0: setAmount(value); break;
             case 1: setMix(value); break;
             case 2: setMode(value); break;
+            case 3: asymmetry = value; break;
+            case 4: stereo_bias = value - 0.5; break;
+            case 5: oversampler.enable(value >= 0.5); break;
         }
     }
 
@@ -563,8 +584,11 @@ public:
         };
         switch (index) {
             default: snprintf(str, sz, "%f", params[index]); break;
-            case 0: snprintf(str, sz, "%f", gain.l); break;
+            case 0: snprintf(str, sz, "%f", gain); break;
             case 2: snprintf(str, sz, "%s", mode_str[mode]); break;
+            case 3: snprintf(str, sz, "%f", asymmetry - 0.5); break;
+            case 4: snprintf(str, sz, "%f", stereo_bias); break;
+            case 5: snprintf(str, sz, "%s", oversampler.enabled() ? "2x" : "none"); break;
         }
     }
     static const char *getParamLabel(int index) { return ""; }
@@ -573,60 +597,64 @@ public:
     virtual void process(SampleStereo *in, size_t n)
     {
         ZoneScopedN("Distortion");
+        const auto range = std::span(in, in+n);
+        // it can go below 0, but that's OK actually.
+        const auto offset = SampleStereo(asymmetry - stereo_bias, asymmetry + stereo_bias);
+
         switch (mode) 
         {
         case Hardclip:
-            for (size_t i = 0; i < n; i++) {
-                auto old = in[i];
-                in[i] = old * dry + max(min(in[i] * gain, 1.0), -1.0) * wet;
-            }
+            oversampler.for_each(range, [this, offset](auto smp) {
+                smp = bias(smp, offset);
+                return max(min(smp * gain, 1.0), -1.0);
+            });
             break;
         case Softclip: {
-            constexpr auto a = -1.42479f;
-            constexpr auto b =  2.20888f;
-            constexpr auto c = -1.02786f;
-            constexpr auto d =  1.13379f;
-            for (size_t i = 0; i < n; i++) {
-                auto old = in[i];
-                auto x = min(1.0, abs(in[i]) * gain);
-                in[i] = old * dry + copysign((x*x*x*x*a + x*x*x*b + x*x*c + x*d), old) * wet;
-            }
+            oversampler.for_each(range, [this, offset](auto smp) {
+                constexpr auto a = -1.42479f;
+                constexpr auto b =  2.20888f;
+                constexpr auto c = -1.02786f;
+                constexpr auto d =  1.13379f;
+                smp = bias(smp, offset);
+                auto x = min(1.0, abs(smp) * gain);
+                return copysign((x*x*x*x*a + x*x*x*b + x*x*c + x*d), smp);
+            });
             break;
         }
         case Softsine:
-            for (size_t i = 0; i < n; i++) {
-                auto old = in[i];
-                auto x = min(1.0f, max(-1.0, old * gain / sqrt2));
-                in[i] = old * dry + fast_sin_halfpi(x) * wet;
-            }
+            oversampler.for_each(range, [this, offset](auto smp) {
+                smp = bias(smp, offset);
+                auto x = min(1.0f, max(-1.0, smp * gain / sqrt2));
+                return fast_sin_halfpi(x);
+            });
             break;
         case Tanh:
-            for (size_t i = 0; i < n; i++) {
-                auto old = in[i];
-                in[i] = old * dry + fast_tanh(old * gain) * wet;
-            }
+            oversampler.for_each(range, [this, offset](auto smp) {
+                smp = bias(smp, offset);
+                return fast_tanh(smp * gain);
+            });
             break;
         case Shape:
-            for (size_t i = 0; i < n; i++) {
-                auto old = in[i];
-                auto x = copysign(max(min(pow(abs(old), SampleStereo(1.0) / gain), 1.0), -1.0), old);
-                in[i] = old * dry + x * wet;
-            }
+            oversampler.for_each(range, [this, offset](auto smp) {
+                auto x = bias(smp, offset);
+                auto xabs = abs(x);
+                auto p = SampleStereo(1.0) / (gain); 
+                auto curve = xabs / (xabs - p * (xabs - 1));
+                return copysign(curve, x);
+            });
             break;
         case Fold:
-            for (size_t i = 0; i < n; i++) {
-                auto old = in[i];
-                auto a = (old * gain - 1.0) / 4.0;
-                auto x = abs(a - floor(a) - 0.5) * 4.0 - 1.0;
-                in[i] = old * dry + x * wet;
-            }
+            oversampler.for_each(range, [this, offset](auto smp) {
+                smp = bias(smp, offset);
+                auto a = (smp * gain - 1.0) / 4.0;
+                return abs(a - floor(a) - 0.5) * 4.0 - 1.0;
+            });
             break;
         case BadFold:
-            for (size_t i = 0; i < n; i++) {
-                auto old = in[i];
-                auto x = fmod(old * gain, 1.0);
-                in[i] = old * dry + x * wet;
-            }
+            oversampler.for_each(range, [this, offset](auto smp) {
+                smp = bias(smp, offset);
+                return fmod(smp * gain, 1.0);
+            });
             break;
         }
     }
